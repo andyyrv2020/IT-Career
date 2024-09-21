dca79f9878069a38d459fe50cd6d942cf28a0d97
andyyrv2020 <andyrashev123@icloud.com>
2024-09-21 11:54:57 +0300

    hidrataciq

---
+++ BackpackDp/Program.cs
@@ -0,0 +1,25 @@
+﻿int sum = int.Parse(Console.ReadLine());
+int[] nominals = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] dp= new int[nominals.Length];
+dp[0] = 0;
+dp[1] = 1;
+
+
+for (int i = 2; i <= sum; i++)
+{
+    int min = int.MaxValue;
+    foreach (var nominal in nominals)
+    {
+        if (i - nominal >= 0)
+        {
+            if (dp[i - nominal] < min)
+            {
+                min = dp[i - nominal];
+            }
+        }
+    }
+}
