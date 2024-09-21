3b7f07e7e9abd9b755075cd8eb98245995b4d99d
andyr <andyr@ANDYLT>
2024-09-21 11:27:27 +0300

    Add project files.

---
+++ FibonacciNum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+int n = int.Parse(Console.ReadLine());
+BigInteger[] dp = new BigInteger[n + 1];
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 1;
+
+for (int i = 3; i <= n; i++)
+{
+    dp[i] = dp[i - 1] + dp[i - 2];
+}
+Console.WriteLine(dp[n]);
